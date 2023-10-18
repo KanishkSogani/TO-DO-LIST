f986de70bdfcbc115f7a87666f273ce5b32e5ace
KanishkSogani <kanishksogani1@gmail.com>
2023-10-18 19:51:05 +0530

    Updated Database

--- app.js
+++ app.js
@@ -1,24 +1,52 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-var app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.set('view engine','ejs');
-app.use(express.urlencoded({extended:true}));
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/todo");
 
-app.use(express.static('public'))
-var example = 'working';
-var items = [];
-
-app.get('/',(req,res)=>{
-    res.render('list',{ejes:items})
+const trySchema = new mongoose.Schema({
+  name: String,
 });
 
-app.post('/',(req,res) => {
-    var item = req.body.ele1;
-    items.push(item);
-    res.redirect('/');
-})
+const item = mongoose.model("task", trySchema);
 
-app.listen(3000,()=>{
-    console.log("Server is running");
-})
+app.get("/", (req, res) => {
+  item
+    .find()
+    .then((result) => {
+      res.render("list", { dayej: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/", (req, res) => {
+  const itemName = req.body.ele1;
+  const todo5 = new item({
+    name: itemName,
+  });
+  todo5.save();
+  res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+  const checked = req.body.checkbox1;
+  item
+    .findByIdAndRemove(checked)
+    .then(() => {
+      console.log("Deleted");
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen("3000", () => {
+  console.log("Server is running");
+});
